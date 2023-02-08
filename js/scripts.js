edcd45c2c021b1f3eebe39085aca00ba5bae2ab5
ntd1hc <ngoan.tranduy@vn.bosch.com>
2023-02-08 14:31:06 +0700

    under developing upto usage section

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,71 @@
 $(document).ready(function() {
+   // Load navigation and footer
+   $("nav").load("./html/navigation.html");
+   $("footer").load("./html/footer.html");
+
    // Load *.html templates for each section 
-   var HTML_TEMPLATES = ["background", "features", "downloads", "usage", 
+   var HTML_CONTENTS = ["background", "features", "downloads", "usage", 
                          "document", "about"];
-   for (section of HTML_TEMPLATES){
-      $(`#${section}-content`).load(`./html/${section}.html`);
+   var i = 0;
+   for (section of HTML_CONTENTS){
+      $(`#${section}`).load(`./html/contents/${section}.html`, function(){
+         if (i==(HTML_CONTENTS.length-1)){
+            // Wait for all templates are loaded completely
+            addListeners();
+         }
+         i++;
+      });
    }
 
+});
+
+function addListeners(){
    // Highlight the active section in the navigation bar
    $('body').scrollspy({ target: '#navbarNav' });
-});
 
-var aboutUsSection = document.getElementById("contacts");
-var mainContentSection = document.getElementById("main-content");
+   // Search button event
+   document.getElementById('search-btn').addEventListener('click', searchHandler);
 
-document.getElementById("contacts-button").addEventListener("click", function() {
-   if (aboutUsSection.style.display === "none"){
-      aboutUsSection.style.display = "block";
-      mainContentSection.style.display = "none";
+   // Image clicking events
+   images = document.getElementsByTagName('img');
+   for (var i=0; i < images.length; i++){
+      images[i].addEventListener('click', zoomImage);
    }
-});
 
-document.getElementById("nav-sections").addEventListener("click", function() {
-   if (mainContentSection.style.display === "none"){
-      mainContentSection.style.display = "block";
-      aboutUsSection.style.display = "none";
+   // Download button events
+   // document.getElementById('download-windows').addEventListener('click', function(){
+   //    downloadInstaller("windows")
+   // });
+   // document.getElementById('download-linux').addEventListener('click', function(){
+   //    downloadInstaller("linux")
+   // });
+
+};
+
+// Handlers for searching 
+function searchHandler(event){
+   var searchText = document.getElementById("search").value;
+   event.preventDefault();
+   console.log(`Under implementing - searching: ${searchText}`)
+};
+
+function searchInput(){
+   var inputText = document.getElementById("search").value;
+   console.log(`Under implementing - input: ${inputText}`);
+};
+
+// Handler for images
+function zoomImage(e){
+   console.log("Zooming image")
+};
+
+// Handler for download
+function downloadInstaller(os){
+   var installerURL = "https://github.com/test-fullautomation/RobotFramework_AIO/suites/10576083020/artifacts/";
+   if (os === "windows"){
+      installerURL += "526613609";
+   } else if (os === "linux"){
+      installerURL += "526613608";
    }
-});
+   window.open(installerURL, "Download");
+}
